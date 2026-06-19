7d979dd44fb628639dbdb7202f7eab57ee2e789d
vitumsowoya <msowoyavitumbiko56@gmail.com>
2026-06-19 16:16:27 +0200

    Added visualisation tools

--- CHIV.sh
+++ CHIV.sh
@@ -42,3 +42,28 @@ mamba install -c conda-forge -c bioconda liftoff
 #check if it is working
 liftoff -h
 
+#transfer the annotations from the reference gff3 to your consensus genome, creating a new gff3
+liftoff -g HIV_ref_2.gff3 -o HIV_annotation.gff3 HIV_consensus.fa HIV_Ref_2.fasta
+
+#to visualise, use python, first install
+from dna_features_viewer import GraphicFeature, GraphicRecord
+
+features = [
+    GraphicFeature(start=801, end=2304, strand=+1, label="gag"),
+    GraphicFeature(start=2096, end=5108, strand=+1, label="pol"),
+    GraphicFeature(start=5052, end=5631, strand=+1, label="vif"),
+    GraphicFeature(start=5570, end=5862, strand=+1, label="vpr"),
+    GraphicFeature(start=5842, end=8436, strand=+1, label="tat"),
+    GraphicFeature(start=5981, end=8665, strand=+1, label="rev"),
+    GraphicFeature(start=6073, end=6322, strand=+1, label="vpu"),
+    GraphicFeature(start=6236, end=8807, strand=+1, label="env"),
+    GraphicFeature(start=8808, end=9429, strand=+1, label="nef"),
+]
+
+record = GraphicRecord(
+    sequence_length=9719,
+    features=features
+)
+
+ax, _ = record.plot(figure_width=15)
+ax.figure.savefig("HIV_genome_map.png", dpi=300)
