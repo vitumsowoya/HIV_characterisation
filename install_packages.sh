97a501f2b823d208565a38420cb8601b30c8d206
vitumsowoya <msowoyavitumbiko56@gmail.com>
2026-07-08 16:34:34 +0200

    update

--- install_packages.sh
+++ install_packages.sh
@@ -1,5 +1,7 @@
 conda install -c bioconda fastqc
 conda install -c bioconda trimmomatic
+
+#create a new environment when installing iva because dependencies crash with fastqc and trimmomatic
 conda create -n iva_env
 conda install -c bioconda iva
 
